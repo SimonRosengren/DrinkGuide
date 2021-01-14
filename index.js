770fb3c99ce9ebc4dd940e02b24534692a904282
simonrosengren <simonrosengren123@gmail.com>
2021-01-14 15:13:37 +0100

    serve react statically

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 8000;
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 require('./startup/routes')(app, express);
 require('./startup/db')();
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+}); 
 
 app.listen(port, () => {
   console.log(`App running and listening on port: ${port}`)
