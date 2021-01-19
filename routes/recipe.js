2b4f8feb08e4c42ba3c059fc0232dc457e92c14b
Simonrosengren <simonrosengren123@gmail.com>
2021-01-19 15:24:30 +0100

    endpoint for adding recipe

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,11 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const Recipe = require("../models/recipe");
+const Ingredient = require('../models/ingredient')
+const Joi = require("joi");
+const ApiError = require("../utils/ApiError");
 
-const express = require('express')
-const router = express.Router()
+const schema = Joi.object({
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+  instructions: Joi.string().required(),
+  ingredients: Joi.array().items(Ingredient)
+});
 
-router.post('/', (req, res) => {
-    try {
-        
-    } catch (error) {
-        // TODO: Error middleware
-    }
-})
+router.post('/', async (req, res, next) => {
+  try {
+    const { name, description, instructions, ingredients } = req.body;
+    const validation = await schema.validateAsync(
+      { name, description, instructions, ingredients }
+    );
+    if (validation.error)
+      throw new ApiError(400, "Wrong parameters", validation.error);
+    const recipe = new Recipe({ name, description, instructions, ingredients });
+    const result = await recipe.save();
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
