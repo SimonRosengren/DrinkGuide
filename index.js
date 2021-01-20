3e62db212a265df86e5e638e870ca256cbbc2540
simonrosengren <simonrosengren123@gmail.com>
2021-01-20 22:58:30 +0100

    serve from build

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 require('./startup/routes')(app, express);
 require('./startup/db')();
 
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-}); 
+// app.get('*', (req,res) =>{
+//   res.sendFile(path.join(__dirname+'/client/public/index.html'));
+// }); 
 
 app.listen(port, () => {
   console.log(`App running and listening on port: ${port}`)
