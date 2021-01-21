788d26e461403f73a652db3f89c2ed60486a8a7b
simonrosengren <simonrosengren123@gmail.com>
2021-01-21 22:38:15 +0100

    serving from buid

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 const port = process.env.PORT || 8000;
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(express.static(path.join('client/build')));
 
 require('./startup/routes')(app, express);
 require('./startup/db')();
 
-// app.get('*', (req,res) =>{
-//   res.sendFile(path.join(__dirname+'/client/public/index.html'));
-// }); 
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirnamem, 'client/public', 'index.html'));
+}); 
 
 app.listen(port, () => {
   console.log(`App running and listening on port: ${port}`)
