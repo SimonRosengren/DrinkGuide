f110cc13e9d7a804b456c00b0f4bd6f01f9ea9ad
simonrosengren <simonrosengren123@gmail.com>
2021-01-21 23:05:26 +0100

    use new gitignore

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-require('dotenv').config()
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 8000;
 const path = require('path')
 
-app.use(express.static(path.join('client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+require('dotenv').config()
 require('./startup/routes')(app, express);
 require('./startup/db')();
 
-app.get('*', (req, res) =>{
-  res.sendFile(path.join(__dirnamem, 'client/public', 'index.html'));
-}); 
+// app.get('*', (req, res) =>{
+//   res.sendFile(path.join(__dirnamem, 'client/public', 'index.html'));
+// }); 
 
 app.listen(port, () => {
   console.log(`App running and listening on port: ${port}`)
