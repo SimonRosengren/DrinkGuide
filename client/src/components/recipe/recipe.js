b985ef56cae9be47481fd993182f487de92ba84f
simonrosengren <simonrosengren123@gmail.com>
2021-08-06 23:05:47 +0200

    drinkpage

--- client/src/components/recipe/recipe.js
+++ client/src/components/recipe/recipe.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styles from "./recipe.module.scss";
 
 export default function Recipe({drink}) {
-    return(<div className='wrapper'>
+    return(<div className={styles.wrapper}>
         <h2>{drink.name}</h2>
         <h4>{drink.description}</h4>
         <p>{drink.instructions}</p>
